9459b761defc30931065df5a718bbb9da240c87d
7znetty <abacusnekketu@gmail.com>
2024-01-30 12:51:55 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import discord
+import random
+client = discord.Client()
+# ランダムで送るメッセージの一覧 ※ここに書き足すことでランダムに選ぶ内容を増やせる
+random_contents = [
+    "にゃーん",
+    "わん！",
+    "コケッコッコー",
+]
+
+@client.event
+async def on_ready():
+    print("on_ready")
+    print(discord.__version__)
+
+
+@client.event
+async def on_message(message):
+    # 送信者がbotである場合は弾く
+    if message.author.bot:
+        return 
+    
+    name = message.author.name
+    # メッセージの本文が 鳴いて だった場合
+    if message.content == "!":
+        # 送信するメッセージをランダムで決める
+        content = name + random.choice(random_contents)
+        # メッセージが送られてきたチャンネルに送る
+        await message.channel.send(content)
+    elif message.content == "おはよう":
+        await message.channel.send("おはよう！！")
+
+
+client.run("MTIwMTU2NTc0NjE4MzAyODc3Ng.GfyGDT.Wx8QXHUqIyKtL0RnkxQgAshTSYVOgAUM-rnraU")
